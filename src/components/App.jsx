d941da92ea67a6db75d3d9079713d08a2bf0284f
victoria2588 <123986940+victoria2588@users.noreply.github.com>
2023-08-24 11:15:16 +0300

    commit 004
    hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,65 +1,125 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { GlobalStyle } from './GlobalStyle';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
     return good + neutral + bad;
   };
 
-  countPositiveFeedbackPercentage() {
-    return this.countTotalFeedback() === 0
+  const countPositiveFeedbackPercentage = () => {
+    return countTotalFeedback() === 0
       ? 0
-      : Math.round((this.state.good / this.countTotalFeedback()) * 100);
-  }
-
-  hadlerFeedback = type => {
-    this.setState(prevState => {
-      return {
-        [type]: prevState[type] + 1,
-      };
-    });
+      : Math.round((good / countTotalFeedback()) * 100);
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const options = ['good', 'neutral', 'bad'];
-    const total = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={options}
-            onLeaveFeedback={this.hadlerFeedback}
-          />
-        </Section>
+  const hadlerFeedback = type => {
+    switch (type) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
 
-        <Section title="Statistics">
-          {total === 0 ? (
-            <Notification message="There is no feedback" />
-          ) : (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positivePercentage}
-            />
-          )}
-          <GlobalStyle />
-        </Section>
-      </>
-    );
-  }
-}
+      default:
+        break;
+    }
+  };
+
+  const options = ['good', 'neutral', 'bad'];
+  const total = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
+
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={options} onLeaveFeedback={hadlerFeedback} />
+      </Section>
+
+      <Section title="Statistics">
+        {total === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        )}
+        <GlobalStyle />
+      </Section>
+    </>
+  );
+};
+
+// export class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   countTotalFeedback = () => {
+//     const { good, neutral, bad } = this.state;
+//     return good + neutral + bad;
+//   };
+
+//   countPositiveFeedbackPercentage() {
+//     return this.countTotalFeedback() === 0
+//       ? 0
+//       : Math.round((this.state.good / this.countTotalFeedback()) * 100);
+//   }
+
+//   hadlerFeedback = type => {
+//     this.setState(prevState => {
+//       return {
+//         [type]: prevState[type] + 1,
+//       };
+//     });
+//   };
+
+//   render() {
+//     const { good, neutral, bad } = this.state;
+//     const options = ['good', 'neutral', 'bad'];
+//     const total = this.countTotalFeedback();
+//     const positivePercentage = this.countPositiveFeedbackPercentage();
+//     return (
+//       <>
+//         <Section title="Please leave feedback">
+//           <FeedbackOptions
+//             options={options}
+//             onLeaveFeedback={this.hadlerFeedback}
+//           />
+//         </Section>
+
+//         <Section title="Statistics">
+//           {total === 0 ? (
+//             <Notification message="There is no feedback" />
+//           ) : (
+//             <Statistics
+//               good={good}
+//               neutral={neutral}
+//               bad={bad}
+//               total={total}
+//               positivePercentage={positivePercentage}
+//             />
+//           )}
+//           <GlobalStyle />
+//         </Section>
+//       </>
+//     );
+//   }
+// }
